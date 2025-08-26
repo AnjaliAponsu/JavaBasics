d52a714e7ba84b842d7a3885df2ace5c26d4d852
Anjali Sewmini <anjalisewmini5@gmail.com>
2025-08-26 22:18:10 +0530

    learn getters and setters

---
+++ 3-GetterAndSetter/src/Main.java
@@ -0,0 +1,9 @@
+ public class Main {
+    public static void main(String[] args) {
+        User user1 = new User();
+        user1.setId(1);
+        user1.setName("Nayana");
+        System.out.println(user1.getId());
+        System.out.println(user1.getName());
+    }
+}
